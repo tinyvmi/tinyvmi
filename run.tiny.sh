26fc1a793f2d129f7143c86e71d63449007b040b
lap1trt <tupipa0929@gmail.com>
2018-03-14 17:49:46 -0400

    run.tiny.sh: check domU before run

--- run.tiny.sh
+++ run.tiny.sh
@@ -1,6 +1,77 @@
 #!/bin/bash
 
+guestXLconfig="/home/smeller/extdisk/lab/vmi/vmidocker/xenGuests/u1432bhvm.cfg"
+
 tinyDir=$(dirname $0)
+
+function hasGuestDom(){
+
+ lists="$(xl list)"
+
+ lines=$(echo "$(xl list)" | wc -l)
+
+ echo "get $lines lines"
+
+ if [ "$lines" -eq 2 ]
+ then
+	echo "No domU guest on Xen"
+	xl list
+	return 0
+ elif [ "$lines" -gt 2 ]
+ then
+	echo "found domU guests"
+	xl list
+	return 1
+ else
+	echo "something wrong, number of domains is less than 1"
+	xl list
+	return 0
+ fi
+
+}
+
+function checkAndCreateGuest(){
+
+ hasGuestDom
+ hasGuest="$?"
+ #echo "hasGuest: $hasGuest"
+
+ try=1
+ maxtry=2
+
+ while [ "$hasGuest" -eq 0 ]
+ do
+  if [ "$try" -gt $maxtry ];then
+     break
+  fi
+  echo "has no guest, now try to create one"
+  echo "create guest using $guestXLconfig"
+  xl create $guestXLconfig
+  hasGuestDom
+  hasGuest="$?"
+  ((try++))
+ done
+
+ if [ "$hasGuest" -eq 0 ]
+ then
+   echo "failed to create guest"
+   return 0
+ fi
+
+ return 1
+
+}
+
+
+checkAndCreateGuest
+success="$?" # return 0 if failed, return 1 if success.
+
+if [ $success -eq 0 ];then
+  exit -1
+fi
+
+
+
 
 cd $tinyDir
 
